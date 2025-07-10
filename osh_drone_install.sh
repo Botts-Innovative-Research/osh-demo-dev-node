a5570d8579bed6257327469c7c8d24a668112898
Cameron S. Hunt <44948425+cam-s-hunt@users.noreply.github.com>
2025-07-10 15:18:00 -0400

    Update osh_drone_install.sh
    Adding & so that commands finish before moving on...

--- osh_drone_install.sh
+++ osh_drone_install.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
 #update/upgrade the system
-sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y &
 
 # install build tools
-sudo apt-get install build-essential cmake git zip unzip -y
+sudo apt-get install build-essential cmake git zip unzip -y &
 
 # install sdkman!
-curl -s "https://get.sdkman.io" | bash
+curl -s "https://get.sdkman.io" | bash &
 
 # source
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+source "$HOME/.sdkman/bin/sdkman-init.sh" &
 
 #install java
-sdk install java 21.0.6-tem
+sdk install java 21.0.6-tem &
 
 
 # Clone OSH Demo Dev Node w/ new Mavlink Driver
-git clone --recursive https://github.com/Botts-Innovative-Research/osh-demo-dev-node.git
+git clone --recursive https://github.com/Botts-Innovative-Research/osh-demo-dev-node.git &
 
 # Build OSH
 cd osh-demo-dev-node
-./gradlew build -x test -x osgi
+./gradlew build -x test -x osgi &
 
 # Clone MAVSDK
 cd ..
-git clone https://github.com/mavlink/MAVSDK.git
+git clone https://github.com/mavlink/MAVSDK.git &
 cd MAVSDK
-git submodule update --init --recursive
+git submodule update --init --recursive &
 
 # Build MAVSDK, including Server
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_MAVSDK_SERVER=ON -Bbuild -S.
-cmake --build build -j8
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_MAVSDK_SERVER=ON -Bbuild -S. &
+cmake --build build -j8 &
 
 # Install MAVSDK, including Server
-sudo cmake --build build --target install
+sudo cmake --build build --target install &
 
 # Update LDCONFIG
-sudo ldconfig
+sudo ldconfig &
 
 # Clone Ardupilot
 cd ..
-git clone https://github.com/ArduPilot/ardupilot.git
+git clone https://github.com/ArduPilot/ardupilot.git &
 
 # Install Ardupilot prereqs
 cd ardupilot
-Tools/environment_install/install-prereqs-ubuntu.sh -y
+Tools/environment_install/install-prereqs-ubuntu.sh -y &
 
 #reload path
-. ~/.profile
+. ~/.profile &
 
 # Add Taiwan as pre-defined location in Ardupilot
-echo "Taiwan=24.178772,120.649633,0,0" >> Tools/autotest/locations.txt
+echo "Taiwan=24.178772,120.649633,0,0" >> Tools/autotest/locations.txt &
 
 # Add additional mav out
-mkdir ~/.mavproxy
-touch ~/.mavproxy/mavinit.scr
+mkdir ~/.mavproxy &
+touch ~/.mavproxy/mavinit.scr &
 echo "output add 127.0.0.1:14540" >> ~/.mavproxy/mavinit.scr
 echo "output add 127.0.0.1:14560" >> ~/.mavproxy/mavinit.scr
 echo "output add 127.0.0.1:14550" >> ~/.mavproxy/mavinit.scr
