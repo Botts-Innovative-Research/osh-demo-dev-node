d1c69a15b1d3f5114c78e756880a34fc820f5776
micstinso <michael.stinson@botts-inc.com>
2025-07-21 15:25:54 -0500

    Splitting mavsdk from ardupilot for containerization

--- osh_drone_install.sh
+++ osh_drone_install.sh
@@ -7,59 +7,10 @@ sudo apt update && sudo apt upgrade -y
 wait
 echo "Sys update/upgrade done"
 
-# set up adoptium
-sudo apt install -y wget apt-transport-https
-wait
-echo "apt-transport-https installed"
-
-sudo wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
-
-sudo echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-wait
-echo "Added adoptium to sources list"
-
-#update again to get adoptium repo
-sudo apt update
-wait
-echo "Updated to get adoptium"
-
-echo "setting JAVA_HOME"
-echo "export JAVA_HOME=$(realpath $(dirname $(readlink -f $(which java)))/../)" >> ~/.bashrc
-
 # install build tools
-sudo apt-get install build-essential cmake git zip unzip python3-pip temurin-17-jdk temurin-21-jdk -y
+sudo apt-get install build-essential cmake git zip unzip python3-pip -y
 wait
 echo "pre-req tools installed"
-
-# Clone MAVSDK
-cd ..
-git clone https://github.com/mavlink/MAVSDK.git
-wait
-echo "MAVSDK cloned"
-
-cd MAVSDK
-git submodule update --init --recursive
-wait
-echo "MAVSDK submodules updated"
-
-# Build MAVSDK, including Server
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_MAVSDK_SERVER=ON -Bbuild -S.
-wait
-echo "MAVSDK build config"
-
-cmake --build build -j8
-wait
-echo "MAVSDK built"
-
-# Install MAVSDK, including Server
-sudo cmake --build build --target install
-wait
-echo "MAVSDK installed"
-
-# Update LDCONFIG
-sudo ldconfig
-wait
-echo "ldconfig updated"
 
 # Clone Ardupilot
 cd ..
@@ -80,6 +31,8 @@ echo "path reloaded"
 
 # Add Taiwan as pre-defined location in Ardupilot
 echo "Taiwan=24.178772,120.649633,0,0" >> Tools/autotest/locations.txt
+echo "Taiwan2=24.178772,120.648633,0,0" >> Tools/autotest/locations.txt
+echo "Taiwan3=24.178772,120.647633,0,0" >> Tools/autotest/locations.txt
 wait
 echo "Taiwan added as location to Ardupilot"
 
